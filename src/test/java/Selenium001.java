19721d70632054a9c006eaa07e8fd2cb3d10494c
MonaliKhode8 <monalikhode028@gmail.com>
2026-04-01 16:50:46 +0530

    Selenium Day1

---
+++ src/test/java/Selenium001.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium001 {
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+    }
+}
